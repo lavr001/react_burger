69b996a673f89cadf2bf71a247f2a81aebef52d6
Roman Lavrenov <lavr001@gmail.com>
2021-03-11 16:19:40 -0800

    toolbar is done

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 import classes from './Layout.module.scss';
 
 const layout = props => (
   <>
     <Toolbar />
+    <SideDrawer />
     <main className={classes.content}>
       {props.children}
     </main>
---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import classes from './SideDrawer.module.scss';
+
+const side_drawer = props => {
+  //...
+  return (
+    <div className={classes.SideDrawer}>
+      <div className={classes.Logo}>
+        <Logo />
+      </div>
+      <nav>
+        <NavigationItems />
+      </nav>
+    </div>
+  )
+}
+
+export default side_drawer;
